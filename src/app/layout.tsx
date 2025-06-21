868e5a8ed0b120876c8f6642400be5ee96346533
Dor-J <Mr.DorJosef@gmail.com>
2025-06-21 22:22:35 +0300

    Prepare app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,25 @@
-import "~/styles/globals.css";
+import '~/styles/globals.css'
 
-import { type Metadata } from "next";
+import { type Metadata } from 'next'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export const metadata: Metadata = {
-  title: "Dor's assignment",
-  description: "Assesment for junior intern assignment",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
+    title: "Dor's assignment",
+    description: 'Assesment for junior intern assignment',
+    icons: [{ rel: 'icon', url: '/favicon.ico' }]
+}
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    )
 }
