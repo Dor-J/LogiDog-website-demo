75d3604cab7bdada7b5972aa1f539afbc6092a96
Dor-J <Mr.DorJosef@gmail.com>
2025-06-22 17:28:49 +0300

    Add styling to navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,18 +1,44 @@
+'use client'
+
+import clsx from 'clsx'
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
 
 function NavBar() {
+    const pathname = usePathname()
+
     return (
         <div>
             <nav className="flex items-center justify-center space-x-4">
                 <ul className="flex space-x-6">
-                    <li>
+                    <li
+                        className={clsx(
+                            'px-4 py-2 hover:text-blue-200 hover:underline' +
+                                (pathname === '/'
+                                    ? ' border-4 border-blue-800'
+                                    : '')
+                        )}
+                    >
                         <Link href="/">Home</Link>
                     </li>
-                    <li>
+                    <li
+                        className={clsx(
+                            'px-4 py-2 hover:text-blue-200 hover:underline' +
+                                (pathname === '/dashboard'
+                                    ? ' border-4 border-blue-800'
+                                    : '')
+                        )}
+                    >
                         <Link href="/dashboard">Dashboard</Link>
                     </li>
-                    <li>
+                    <li
+                        className={clsx(
+                            'px-4 py-2 hover:text-blue-200 hover:underline' +
+                                (pathname === '/contact'
+                                    ? ' border-4 border-blue-800'
+                                    : '')
+                        )}
+                    >
                         <Link href="/contact">Contact</Link>
                     </li>
                 </ul>
