ade243878ea6d17232fbd3b4b2c322d9fdd9b150
Dor-J <Mr.DorJosef@gmail.com>
2025-06-22 21:32:46 +0300

    Add high order component for shipment index

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import AtRiskShipments from './components/AtRiskShipments'
 
 function DashBoard() {
     return (
         <main className="flex h-lvh w-full flex-col items-center">
             <div className="p4 text-center">
-                <h1>Dashboard</h1>
+                <h1>Shipments Delay Dashboard</h1>
             </div>
+
+            {/* Layout: sidebar + table */}
+            <AtRiskShipments />
         </main>
     )
 }
