a11b3ad229ab0c8e8a6cc22061837a09a6eefb31
Dor-J <Mr.DorJosef@gmail.com>
2025-06-22 22:05:50 +0300

    Add styling to the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,11 @@ import AtRiskShipments from './components/AtRiskShipments'
 
 function DashBoard() {
     return (
-        <main className="flex h-lvh w-full flex-col items-center">
-            <div className="p4 text-center">
-                <h1>Shipments Delay Dashboard</h1>
+        <main className="flex w-full flex-col items-center">
+            <div className="p4 my-4 text-center">
+                <h1 className="text-3xl font-bold">
+                    Shipments Delay Dashboard
+                </h1>
             </div>
 
             {/* Layout: sidebar + table */}
