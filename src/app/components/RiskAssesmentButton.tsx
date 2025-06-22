c600bb7c86582c0828e6cdd350397aff4f7d098e
Dor-J <Mr.DorJosef@gmail.com>
2025-06-23 02:26:19 +0300

    Add RiskAssesmentButton component for shipment risk assessment

---
+++ src/app/components/RiskAssesmentButton.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+import { isShipmentAtRiskTest } from '../../utils/shipmentRisk'
+
+function RiskAssesmentButton() {
+    return (
+        <button
+            className="rounded-2xl bg-emerald-300 px-5 py-2 text-black transition-all hover:bg-emerald-500"
+            onClick={() => isShipmentAtRiskTest()}
+        >
+            Risk Assesment Button
+        </button>
+    )
+}
+
+export default RiskAssesmentButton
