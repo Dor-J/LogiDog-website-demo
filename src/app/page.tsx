ad664473f24b2b0cd4c9614e2591b0e9b7e02e5d
Dor-J <Mr.DorJosef@gmail.com>
2025-06-23 02:26:47 +0300

    Add RiskAssesmentButton component to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero'
 import ShipmentDelayFlow from './components/ShipmentDelayFlow'
+import RiskAssesmentButton from './components/RiskAssesmentButton'
 
 export default function HomePage() {
     return (
         <main className="flex w-full flex-col items-center">
             <Hero />
             <ShipmentDelayFlow />
+            <RiskAssesmentButton />
         </main>
     )
 }
