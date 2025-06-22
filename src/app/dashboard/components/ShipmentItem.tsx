bb489b57c15853a5b42273276f696f013d2ad9b9
Dor-J <Mr.DorJosef@gmail.com>
2025-06-22 22:22:19 +0300

    Enhance ShipmentItem component to display detailed shipment information with accordion functionality

--- src/app/dashboard/components/ShipmentItem.tsx
+++ src/app/dashboard/components/ShipmentItem.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 function ShipmentItem() {
-    return <div>ShipmentItem</div>
+    return (
+        <div>
+            <p>
+                ShipmentItem will be used to display the details of a shipment
+                item opens up as accordion for faster retrival
+            </p>
+        </div>
+    )
 }
 
 export default ShipmentItem
