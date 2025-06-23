f9b362a7d9027fc3e0c5b7e3ecd6c93ae414aeda
Dor-J <Mr.DorJosef@gmail.com>
2025-06-23 23:34:18 +0300

    Refactor NavBar component to use dynamic routing for improved maintainability

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -7,30 +7,23 @@ import { usePathname } from 'next/navigation'
 function NavBar() {
     const pathname = usePathname()
 
+    const routes = { '/': 'Home', '/dashboard': 'Dashboard' }
+
     return (
         <div>
             <nav className="flex items-center justify-center space-x-4">
                 <ul className="flex space-x-6">
-                    <li
-                        className={clsx(
-                            'border-4 border-transparent px-4 py-2 hover:text-blue-200 hover:underline' +
-                                (pathname === '/'
-                                    ? ' border-4 border-blue-800'
-                                    : '')
-                        )}
-                    >
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li
-                        className={clsx(
-                            'border-4 border-transparent px-4 py-2 hover:text-blue-200 hover:underline' +
-                                (pathname === '/dashboard'
-                                    ? ' border-blue-800'
-                                    : '')
-                        )}
-                    >
-                        <Link href="/dashboard">Dashboard</Link>
-                    </li>
+                    {Object.entries(routes).map(([path, label]) => (
+                        <li
+                            key={path}
+                            className={clsx(
+                                'border-4 border-transparent px-4 py-2 hover:text-blue-200 hover:underline',
+                                pathname === path ? 'border-blue-800' : ''
+                            )}
+                        >
+                            <Link href={path}>{label}</Link>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </div>
